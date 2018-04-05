e780e7d96edee4a9b2d0d5b68c339fea2b5ac1d4
Kybox <nslr@riseup.net>
2018-04-05 11:40:19 +0200

    Utilisation EntityManager + persistence.xml (au lieu de SessionFactory + hibernate.cfg.xml)

--- kyclimber/business/src/main/java/fr/kybox/impl/manager/UserManagerImpl.java
+++ kyclimber/business/src/main/java/fr/kybox/impl/manager/UserManagerImpl.java
@@ -3,12 +3,17 @@ package fr.kybox.impl.manager;
 import fr.kybox.bean.user.User;
 import fr.kybox.exception.NotFoundException;
 import fr.kybox.interfaces.manager.UserManager;
+import fr.kybox.util.HibernateUtil;
 import fr.kybox.util.MD5;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.List;
 
 
@@ -16,63 +21,54 @@ import java.util.List;
  * @author Kybox
  * @version 1.0
  */
+
 @Service
 public class UserManagerImpl extends AbstractManager implements UserManager {
 
-    private SessionFactory factory;
-
-    private Session getSession(){
-
-        factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(User.class).buildSessionFactory();
-        return factory.getCurrentSession();
-    }
+    private final EntityManager entityManager = HibernateUtil.getEntityManager();
 
     @Override
     public List<User> getUserList() {
 
-        Session session = getSession();
         List<User> userList;
 
-        try {
-            session.beginTransaction();
-            userList = session.createQuery("from User").list();
-            session.getTransaction().commit();
-        }
-        finally {
-            factory.close();
-        }
+        entityManager.getTransaction().begin();
+
+        Query query = entityManager.createQuery("select u from User u");
+        userList = query.getResultList();
+
+        entityManager.getTransaction().commit();
 
         return userList;
     }
 
     @Override
     public User getUser(String login, String pass) throws NotFoundException {
-        pass = MD5.hash(pass);
-        Session session = getSession();
-        List<User> userList;
-        try {
-            session.beginTransaction();
-            String query = "from User u where u.email = :login and u.password = :pass";
-            userList = session.createQuery(query).setParameter("login", login).setParameter("pass", pass).list();
-        }
-        finally { factory.close(); }
 
+        pass = MD5.hash(pass);
+
+        List<User> userList;
+        entityManager.getTransaction().begin();
+        String query = "from User u where u.email = :login and u.password = :pass";
+        userList = entityManager.createQuery(query).setParameter("login", login).setParameter("pass", pass).getResultList();
+        entityManager.getTransaction().commit();
         if(userList.isEmpty()) throw new NotFoundException();
         else return userList.get(0);
     }
 
     @Override
     public void addNewUser(User user) {
-
-        Session session = getSession();
+        /*
+        Session session = HibernateUtil.getSession();
         try {
             session.beginTransaction();
             session.save(user);
             session.getTransaction().commit();
         }
         finally {
-            factory.close();
+            session.close();
         }
+        */
     }
 
 }
--- kyclimber/business/src/main/java/fr/kybox/util/HibernateUtil.java
+++ kyclimber/business/src/main/java/fr/kybox/util/HibernateUtil.java
@@ -1,30 +1,38 @@
 package fr.kybox.util;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 
 /**
- * @author Kybox
- * @version 1.0
+ * @author Deepak Kumar
+ * Web: http://www.roseindia.net
  */
 public class HibernateUtil {
 
-    private static SessionFactory sessionFactory;
-    private static Configuration configuration;
+    private static final String PERSISTENCE_UNIT = "kyclimber";
+    private static EntityManagerFactory entityManagerFactory;
+    private static EntityManager entityManager;
 
-    private static void setFactory(){
+    private static void setEntityManager(){
 
-        configuration = new Configuration().configure("hibernate.cfg.xml");
-        sessionFactory = configuration.buildSessionFactory();
+        entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+        entityManager = entityManagerFactory.createEntityManager();
     }
+    public static EntityManager getEntityManager(){
 
-    public static SessionFactory getSessionFactory() {
+        if(entityManager == null) setEntityManager();
 
-        if(sessionFactory == null) setFactory();
-        return sessionFactory;
+        return entityManager;
     }
 }
