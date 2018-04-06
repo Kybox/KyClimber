bab541ef2626667954e9fb2c20301e857e7d4aa6
Kybox <nslr@riseup.net>
2018-04-06 22:48:51 +0200

    Ajout des attributs nécessaires aux sites

--- kyclimber/model/src/main/java/fr/kybox/entities/Site.java
+++ kyclimber/model/src/main/java/fr/kybox/entities/Site.java
@@ -23,6 +23,36 @@ public class Site {
     @Column(name = "department")
     private String department;
 
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "height")
+    private String height;
+
+    @Column(name = "orientation")
+    private String orientation;
+
+    @Column(name = "route")
+    private String route;
+
+    @Column(name = "quotation")
+    private String quotation;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "type_description")
+    private String typeDescription;
+
+    @Column(name = "access")
+    private String access;
+
+    @Column(name = "topo")
+    private String topo;
+
+    @Column(name = "map")
+    private String map;
+
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
 
@@ -34,4 +64,34 @@ public class Site {
 
     public String getDepartment() { return department; }
     public void setDepartment(String department) { this.department = department; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getHeight() { return height; }
+    public void setHeight(String height) { this.height = height; }
+
+    public String getOrientation() { return orientation; }
+    public void setOrientation(String orientation) { this.orientation = orientation; }
+
+    public String getRoute() { return route; }
+    public void setRoute(String route) { this.route = route; }
+
+    public String getQuotation() { return quotation; }
+    public void setQuotation(String quotation) { this.quotation = quotation; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getTypeDescription() { return typeDescription; }
+    public void setTypeDescription(String typeDescription) { this.typeDescription = typeDescription; }
+
+    public String getAccess() { return access; }
+    public void setAccess(String access) { this.access = access; }
+
+    public String getTopo() { return topo; }
+    public void setTopo(String topo) { this.topo = topo; }
+
+    public String getMap() { return map; }
+    public void setMap(String map) { this.map = map; }
 }
